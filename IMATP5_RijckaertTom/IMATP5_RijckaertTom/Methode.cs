1ed8102e4af4a40a01d71d7be7c7f8f139c1d050
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-04-18 10:12:36 +0200

    interro matrice (on est pas dans la merde)

---
+++ IMATP5_RijckaertTom/IMATP5_RijckaertTom/Methode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMATP5_RijckaertTom
+{
+    public struct Methode
+    {
+        public void Etape1(int _taille, out int[,] _Matrice)
+        {
+            _Matrice  = new int[_taille, _taille];
+            _Matrice[1, 1] = 1;
+            for (int ligne = 1; ligne < _taille; ligne++)
+            {
+                for (int colonne = 1; colonne < _taille; colonne++)
+                {
+                    _Matrice[ligne, colonne] = _Matrice[ligne - 1, colonne - 2] + _Matrice[ligne - 1, colonne - 2];
+                }
+            }
+        }
+
+        public void Etape2(int[,] _Matrice, int _taille, out string _StringMatrice)
+        {
+            int nbLigne = _Matrice.GetLength(0);
+            int nbColonne = _Matrice.GetLength(1);
+            _StringMatrice = "(a + b)^" + _taille + " =";
+            for (int ligne = 1; ligne < nbLigne; ligne++)
+            {
+                for (int colonne = 1; colonne < nbColonne; colonne++)
+                {
+                    _StringMatrice += "" + _Matrice[ligne, colonne] + "a^" + _taille + "b";
+                }
+            }
+        }
+        public void String_Tableau(int[,] Tab, out string stringTab)
+        {
+            // instanciation de la variable stringTab
+            stringTab = "";
+            // boucle pour se balades dans les lignes de la matrice
+            for (int ligne = 0; ligne < Tab.GetLength(0); ligne++)
+            {
+                // boucle pour se balader dans les colonnes de la matrice
+                for (int colonne = 0; colonne < Tab.GetLength(1); colonne++)
+                {
+                    // mise des données de la matrice dans la variable string stringTab
+                    stringTab += Tab[ligne, colonne] + "|";
+                }
+                // passage à la ligne
+                stringTab += "\n";
+            }
+        }
+
+    }
+}
---
+++ IMATP5_RijckaertTom/IMATP5_RijckaertTom/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IMATP5_RijckaertTom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Methode mesOutils = new Methode();
+                Console.WriteLine("BIENVENUE !");
+
+                Console.WriteLine("Quel est la taille du tableau ? ");
+                int _taille = int.Parse(Console.ReadLine());
+
+                //mesOutils.Etape1(_taille, out int[,] _Matrice);
+
+                //mesOutils.String_Tableau(_Matrice, out string stringTab);
+                //Console.WriteLine(stringTab);
+
+                //Console.WriteLine("Voulez vous afficher l'étape de développement de l'analyse n°2 ? ");
+                //string a = Console.ReadLine();
+                //if (a == "oui")
+                //{
+                // mesOutils.Etape2(_Matrice, _taille, out string _StringMatrice);
+                //}
+                Console.WriteLine("Voulez-vous recommencer ?");
+                string b = Console.ReadLine();
+                if (b != "oui")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
